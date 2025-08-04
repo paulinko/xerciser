abfd159ed38a6784f141a3765f8a38689c559d90
[dyad] <git@dyad.sh>
2025-08-04 18:09:32 +0200

    [dyad] wrote 1 file(s), deleted 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,11 +1,9 @@
-import { MadeWithDyad } from "@/components/made-with-dyad";
 import WorkoutTimer from "@/components/WorkoutTimer";
 
 const Index = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-background">
       <WorkoutTimer />
-      <MadeWithDyad />
     </div>
   );
 };
