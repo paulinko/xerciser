27cec96a215fe45948cdc42f664e8418bfae96ae
[dyad] <git@dyad.sh>
2025-08-04 19:01:47 +0200

    [dyad] Fixing local development after GitHub Pages basename change - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Credits from "./pages/Credits"; // Import the new Credits page
+import Credits from "./pages/Credits";
 
 const queryClient = new QueryClient();
 
@@ -14,10 +14,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/xerciser/"> {/* Added basename for GitHub Pages */}
+      <BrowserRouter basename={import.meta.env.BASE_URL}> {/* Use dynamic base URL */}
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/credits" element={<Credits />} /> {/* Add the new Credits route */}
+          <Route path="/credits" element={<Credits />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
